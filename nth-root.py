b7c498f8150c995858d10ddf037b47267970a028
QuentinKamenda <quentin.kamenda@isen.yncrea.fr>
2017-12-19 12:01:21 +0100

    Adding the negative number handle

--- nth-root.py
+++ nth-root.py
@@ -9,7 +9,7 @@ a = float(input("Enter a number: "))
 b = int(input("Enter an integer from 2 to 10: "))
 #e = float(input("Enter the wanted precision: "))
 
-# Function to calculate the n-th root of k with a precision e
+# Calculus of the n-th root of k with a precision e
 # (u0 represents the starting point, some starting poitns are optimum for specific roots)
 def nth_root(k, n, u0, e):
     # Initialisation
@@ -25,12 +25,30 @@ def nth_root(k, n, u0, e):
 # Function to calculate a power b (a^b):
 def power(a, b):
     s = a
-    for i in range(1, b):
+    for i in range(1, b) :
         s = s*a
     return s
 
+# Function to calculate the n-th root of k with a precision e
+# Uses the "nth_root" function
+def calculate(k, n, u0, e):
+    if (a > 0) :
+        # Calculates the root for a positive number
+        return nth_root(k, n, u0, e)
+    else :
+        # Calculates the root for a negative number
+        if (n%2 == 0) :
+            # If the number is even, there is only complex roots
+            return 0
+        else :
+            # If the power is odd, the calculus is the same as for a positive number (* -1)
+            k = k*-1
+            u0 = k
+            return -1 * nth_root(k, n, u0, e)
+
+
 # Result:
-s = nth_root(a, b, a, 0.01)
+s = calculate(a, b, a, 0.01)
 
 print ("The ",b,"-th root of ",a," is: ",s)
 
